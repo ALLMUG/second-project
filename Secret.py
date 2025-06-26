b94824cd078f50f539f6a0764933793d4c84f7e9
ALLMUG <muigaiallan45@gmail.com>
2025-06-26 19:48:41 +0300

    geussing the scret number

---
+++ Secret.py
@@ -0,0 +1,9 @@
+secret_number=3456
+secret_number=int(input("Enter secret number"))
+while secret_number!=3456:
+    print("you are stuck in my loop")
+    secret_number=int(input("Enter secret number"))
+    secret_number==3456
+    print("you got the secret number")
+
+    
